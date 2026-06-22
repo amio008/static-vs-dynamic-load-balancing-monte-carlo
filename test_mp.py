20e14cc5ade8c5b548316a34ffc5e52338399e1e
Amio Ankon <amiomalakar744@gmail.com>
2026-06-22 12:42:31 +0600

    Add files via upload

---
+++ test_mp.py
@@ -0,0 +1,10 @@
+# test_mp.py
+from multiprocessing import Pool
+import os
+
+def f(x):
+    return x * x
+
+if __name__ == "__main__":
+    with Pool(os.cpu_count()) as p:
+        print(p.map(f, [1, 2, 3, 4]))
